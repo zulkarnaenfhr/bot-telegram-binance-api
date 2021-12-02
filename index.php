da036cb3a2caf6905c6910b72cbf2b96912bf1d8
Fahri Izuddin Zulkarnaen <fahrizulkarnaen1@gmail.com>
2021-12-02 07:15:30 +0700

    tambah penjelasan

--- index.php
+++ index.php
@@ -1,33 +1,33 @@
 <?php
-$content = file_get_contents("php://input");
+$content = file_get_contents("php://input"); //Mengambil data Raw JSON
 if($content){
-    $token = '2103447613:AAH5D-aZ5LolbrI-u8DG9fYbf2oVhIPYp6U';
+    $token = '2103447613:AAH5D-aZ5LolbrI-u8DG9fYbf2oVhIPYp6U'; //Token Bot
     
-    $apiLink = "https://api.telegram.org/bot$token/";
+    $apiLink = "https://api.telegram.org/bot$token/"; //API Link untuk bot Telegram
     
-    echo '<pre>content = '; print_r($content); echo '</pre>';
-    $update = json_decode($content, true);
-    if(!@$update["message"]) $val = $update['callback_query'];
-    else $val = $update;
+    echo '<pre>content = '; print_r($content); echo '</pre>'; //Melakukan print array. Digunakan untuk mempermudah membaca data array json.
+    $update = json_decode($content, true); //Pada variable $update digunakan untuk mengubah data Json menjadi php
+    if(!@$update["message"]) $val = $update['callback_query']; //Jika tidak ada error pada variable update maka variable val akan membaca data message dari $content
+    else $val = $update; //Jika pesan yang terbaru sudah dibaca atau belum ada pesan lain, maka program akan menunggu pesan yang baru
     
-    $chat_id = $val['message']['chat']['id'];
-    $text = $val['message']['text'];
-    $update_id = $val['update_id'];
-    $sender = $val['message']['from'];
+    $chat_id = $val['message']['chat']['id']; //Mengambil data chat_id untuk mengirim pesan kepada pengguna
+    $text = $val['message']['text']; //Mengambil data pesan yang dikirim oleh pengguna
+    $update_id = $val['update_id']; //Mengambil data update apakah pesan paling baru atau tidak
+    $sender = $val['message']['from']; //Mengambil data pesan dan pengirim
     ?>
     
     <?php 
-    $data = json_decode(file_get_contents("https://api.binance.me/api/v3/ticker/24hr"), TRUE);
-    if($text=="/view"){
+    $data = json_decode(file_get_contents("https://api.binance.me/api/v3/ticker/24hr"), TRUE); //Digunakan untuk mengambil data json API dari binance
+    if($text=="/view"){ //Digunakan untuk mendeteksi pesan yang dikirim pengguna, apakah command atau hanya sebatas pesan random
         $a = 1;
         $msg .= "Bot Developed by Kelompok API 7!%0a%0a%0a";
         for($i=0;$i<=13;$i++){
-        $asset = $data[$i]['symbol'];
-        $lastPrice = $data[$i]['lastPrice'];
-        $highPrice = $data[$i]['highPrice'];
-        $lowPrice = $data[$i]['lowPrice'];
-        $lastHigh = $highPrice-$lastPrice;
-        $lastLow = $lastPrice-$lowPrice;
+        $asset = $data[$i]['symbol'];//Mengambil data asset dan pair pada api binance
+        $lastPrice = $data[$i]['lastPrice']; // Mengambil data harga terakhir pada api binance
+        $highPrice = $data[$i]['highPrice']; // Mengambil data harga tertinggi pada api binance
+        $lowPrice = $data[$i]['lowPrice']; // Mengambil data harga terendah pada api binance
+        $lastHigh = $highPrice-$lastPrice; // Mengambil data range dari harga terakhir ke tertinggi
+        $lastLow = $lastPrice-$lowPrice; // Mengambil data range dari harga terakhir ke terendah
 
         $tb .= 'No.'.$a.
             '%0aAsset : '.$asset.
@@ -39,7 +39,7 @@ if($content){
             '%0a%0a';
             $a++;
         }
-        file_get_contents($apiLink . "sendmessage?chat_id=$chat_id&text=".$msg.$tb."...");
+        file_get_contents($apiLink . "sendmessage?chat_id=$chat_id&text=".$msg.$tb."..."); //Digunakan untuk mengirimkan pesan kepada pengguna
         for($i=14;$i<=30;$i++){
             $asset = $data[$i]['symbol'];
             $lastPrice = $data[$i]['lastPrice'];
